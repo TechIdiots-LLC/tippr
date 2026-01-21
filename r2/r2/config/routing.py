e23322e2aa78379d3743597a4237a3f959fbadca
acalcutt <acalcutt@techidiots.net>
2026-01-20 23:22:06 -0500

    update code url

--- r2/r2/config/routing.py
+++ r2/r2/config/routing.py
@@ -448,7 +448,7 @@ def make_map(config):
        controller="mediaembed", action="mediaembed", credentials=None)
 
     mc('/code', controller='redirect', action='redirect',
-       dest='http://github.com/reddit/')
+       dest='https://github.com/TechIdiots-LLC/tippr')
 
     mc('/socialite', controller='redirect', action='redirect',
        dest='https://addons.mozilla.org/firefox/addon/socialite/')
